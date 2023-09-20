befbf60464e96cec04c4fff01e2295b862c8d4d2
hjoo <hjoo@dodoom.co.kr>
2023-09-20 17:04:02 +0900

    feat: 상품 추가 데이터 바인딩

--- src/pages/CreateProduct/index.tsx
+++ src/pages/CreateProduct/index.tsx
@@ -1,6 +1,38 @@
+import { useState } from 'react';
 import GeneralWrap from '../../components/GeneralWrap';
 
 export default function CreateProduct() {
+
+    const [imgUrl, setImgUrl] = useState<any>('');
+
+    const onChangeFile = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const file = (e.currentTarget.files as FileList)[0];
+        const reader = new FileReader();
+
+        if (file) {
+            reader.readAsDataURL(file);
+        }
+        reader.onload = () => {
+            setImgUrl(reader.result);
+        }
+    }
+
+    const handleCreateData = (e: any) => {
+        e.preventDefault();
+
+        let uuid = crypto.randomUUID();
+        const data = {
+            id:             uuid,
+            productName:    e.target['productName'].value,
+            price:          Number(e.target['price'].value),
+            likeItTotal:    0,
+            description:    e.target['description'].value,
+            productUrl:     imgUrl
+        }
+
+        console.log("data", data)
+    };
+
     return (
         <section>
             <GeneralWrap>
@@ -8,7 +40,12 @@ export default function CreateProduct() {
                     <h2 className="text-2xl text-center font-bold">
                         Create Product🧷
                     </h2>
-                    <form className="w-full mt-10 grid gap-4">
+                    <form
+                        className="w-full mt-10 grid gap-4"
+                        onSubmit={(e) => {
+                            handleCreateData(e);
+                        }}
+                    >
                         <div className="w-full md:w-1/2 my-0 mx-auto">
                             <label htmlFor="productName" className="hidden">
                                 productName
@@ -16,6 +53,7 @@ export default function CreateProduct() {
                             <input
                                 type="text"
                                 id="productName"
+                                name="productName"
                                 placeholder="productName"
                                 className="w-full py-2 px-4 border rounded-lg focus:shadow-sm focus:shadow-blue-200 focus:outline-none focus:border-blue-400 placeholder:italic placeholder:text-slate-400"
                             ></input>
@@ -24,20 +62,22 @@ export default function CreateProduct() {
                             <label htmlFor="description" className="hidden">
                                 description
                             </label>
-                            <input
-                                type="text"
+                            <textarea
                                 id="description"
+                                name="description"
                                 placeholder="description"
+                                rows={5}
                                 className="w-full py-2 px-4 border rounded-lg focus:shadow-sm focus:shadow-blue-200 focus:outline-none focus:border-blue-400 placeholder:italic placeholder:text-slate-400"
-                            ></input>
+                            ></textarea>
                         </div>
                         <div className="w-full md:w-1/2  my-0 mx-auto">
                             <label htmlFor="price" className="hidden">
                                 price
                             </label>
                             <input
-                                type="text"
+                                type="number"
                                 id="price"
+                                name="price"
                                 placeholder="price"
                                 className="w-full py-2 px-4 border rounded-lg focus:shadow-sm focus:shadow-blue-200 focus:outline-none focus:border-blue-400 placeholder:italic placeholder:text-slate-400"
                             ></input>
@@ -48,9 +88,12 @@ export default function CreateProduct() {
                             </label>
                             <input
                                 type="file"
+                                accept=".jpg,.jpeg,.png,.gif"
                                 id="productUrl"
+                                name="productUrl"
                                 placeholder="productUrl"
                                 className="w-full"
+                                onChange={(e)=>onChangeFile(e)}
                             ></input>
                         </div>
 
