ffc0240f119e063c509362907769cab786e355f7
hjoo <hjoo@dodoom.co.kr>
2023-09-20 15:17:07 +0900

    test: firebase에서 데이터 가져오기 테스트

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -6,14 +6,24 @@ import PriceBar from '../../components/PriceBar';
 import GeneralWrap from '../../components/GeneralWrap';
 
 import { fireStore } from '../../lib/Firebase';
+import { collection, query, where, getDocs } from 'firebase/firestore';
 
 export default function Home() {
     const [foodArr, setFoodArr] = useState<Array<foodDataType>>(foodList);
     const [pickFoodList, setPickFoodList] = useState<Array<foodDataType>>([]);
 
-      useEffect(()=>{
-        console.log("fireStore", fireStore)
-    },[pickFoodList])
+    const getProductOfFireBase = async () => {
+        // 'product' 컬렉션의 모든 문서들을 가져옴
+        const querySnapshot = await getDocs(collection(fireStore, 'product'));
+        querySnapshot.forEach((doc) => {
+            // 가져온 모든 문서들을 확인
+            console.log(doc.id, ' => ', doc.data());
+        });
+    };
+
+    useEffect(() => {
+        getProductOfFireBase();
+    }, []);
 
     return (
         <section>
