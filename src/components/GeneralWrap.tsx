93cc709cb63005fd4dc899ae27f1595ffe34de98
hjoo <hjoo@dodoom.co.kr>
2023-09-20 13:05:09 +0900

    feat: 중앙 정렬 wrap 컴포넌트 생성

---
+++ src/components/GeneralWrap.tsx
@@ -0,0 +1,13 @@
+import { ReactNode } from 'react';
+
+interface PropsType {
+    children: ReactNode;
+}
+
+export default function GeneralWrap({ children }: PropsType) {
+    return (
+        <div className="max-w-[1200px] my-0 mx-auto px-4 border border-blue-600">
+            {children}
+        </div>
+    );
+}
