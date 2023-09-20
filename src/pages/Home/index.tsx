f167de296065c9d1fe69990242bb4fdee2e1e41b
jiwoo Hong <hjw0329@naver.com>
2023-09-20 19:28:07 +0900

    feat: 상품 내역 페이지 firebase 데이터 연결

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,24 +1,29 @@
 import React, { useState, useEffect } from 'react';
 
-import foodList from '../../databases/foodList.json';
+// import foodList from '../../databases/foodList.json';
 import ProductBox from '../../components/ProductBox';
 import PriceBar from '../../components/PriceBar';
 import GeneralWrap from '../../components/GeneralWrap';
 
 import { fireStore } from '../../lib/Firebase';
-import { collection, query, where, getDocs } from 'firebase/firestore';
+import { collection, getDocs } from 'firebase/firestore';
+import { DocumentData } from '@google-cloud/firestore';
 
 export default function Home() {
-    const [foodArr, setFoodArr] = useState<Array<foodDataType>>(foodList);
+    const [foodList, setFoodList] = useState<Array<foodDataType>>([]);
     const [pickFoodList, setPickFoodList] = useState<Array<foodDataType>>([]);
 
     const getProductOfFireBase = async () => {
         // 'product' 컬렉션의 모든 문서들을 가져옴
         const querySnapshot = await getDocs(collection(fireStore, 'product'));
-        querySnapshot.forEach((doc) => {
+
+        const foodArr: foodDataType[] = [];
+        querySnapshot.forEach((doc: DocumentData) => {
             // 가져온 모든 문서들을 확인
-            console.log(doc.id, ' => ', doc.data());
+            // console.log(doc.id, ' => ', doc.data());
+            return foodArr.push(doc.data());
         });
+        setFoodList(foodArr);
     };
 
     useEffect(() => {
@@ -29,7 +34,7 @@ export default function Home() {
         <section>
             <GeneralWrap>
                 <div className="flex md:grid flex-col md:flex-row md:grid-cols-3 lg:grid-cols-4 flex-wrap gap-6 mt-5 mb-32">
-                    {foodArr?.map((item: foodDataType, idx) => (
+                    {foodList?.map((item: foodDataType, idx) => (
                         <React.Fragment key={idx}>
                             <ProductBox
                                 data={item}
